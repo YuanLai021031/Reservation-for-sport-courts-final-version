f524a2c24131c9ea8f891f6fb2c617786052bfea
Lai Yuan <yuanlai021031@gmail.com>
2023-04-19 22:16:29 +0300

    First commit

---
+++ Reservation for sport courts final version/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPFLoginRegisterDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeBackgroundColor(Colors.LightBlue);
+        }
+
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeBackgroundColor(Colors.LightBlue);
+        }
+
+        private void ChangeBackgroundColor(Color color)
+        {
+            var brush = new SolidColorBrush(color);
+            brush.Freeze();
+            this.Dispatcher.Invoke(() => {
+                this.Background = brush;
+            });
+        }
+
+        private void btnLogin_Click()
+        {
+
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
